a1e09b0a516d9cc58bbac105aa399e31038f79b7
Frederic Langevin <frederic.langevin@coopengo.com>
2017-05-15 12:00:35 +0200

    fix sql_type on field function in fail payment process

--- payment.py
+++ payment.py
@@ -283,8 +283,8 @@ class Payment:
     def search_end_to_end_id(cls, name, domain):
         table = cls.__table__()
         _, operator, value = domain
-        cast = cls.sepa_end_to_end_id.sql_type().base
-        Operator = fields.SQL_OPERATORS[operator]
+        # FLA: Temp waiting for tryton 6503
+        cast = cls.sepa_end_to_end_id._field.sql_type().base
         query = table.select(table.id,
             where=Operator(table.id.cast(cast), value))
         return [('id', 'in', query)]
@@ -665,8 +665,8 @@ class Message(Workflow, ModelSQL, ModelView):
                             [table.message, table.type, table.company,
                                 table.origin, table.state],
                             [[message, 'out', company_id,
-                                    'account.payment.group,%s' % group_id,
-                                    'done']]))
+                              'account.payment.group,%s' % group_id,
+                              'done']]))
                 group_table.drop_column('sepa_message')
 
     @staticmethod
